926f06e855a2e042ea9c458c912ab04eb7d24e83
Denis Zhuravlev <d_a_zhuravlev@inbox.ru>
2022-02-26 15:53:18 +0400

    fix MyIterator, add map sort method

--- src/CharacterCounter.java
+++ src/CharacterCounter.java
@@ -1,7 +1,12 @@
 //CharacterCounter
 
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class CharacterCounter {
@@ -16,6 +21,25 @@ public class CharacterCounter {
             else map.put( string.charAt(i), ++n );
         }
 
-        System.out.println(map);
+        System.out.println(sortByValue(map));
+
+    }
+//SORT
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map ) {
+        List<Map.Entry<K, V>> list =
+            new LinkedList<>(map.entrySet());
+        Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+            @Override
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return (o1.getValue()).compareTo( o2.getValue() );
+            }
+        } );
+        Collections.reverse(list);
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list)
+        {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
     }
 }
